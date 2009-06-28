82f40a10e1d3eeeca7653896857e2a2eb0fde98d
Matt Sparks <ms@quadpoint.org>
2009-06-27 18:23:30 -0700

    Documenting speeds

--- alphasign/speeds.py
+++ alphasign/speeds.py
@@ -1,3 +1,24 @@
+"""
+The following constants are defined to change speeds in TEXT
+(:class:`alphasign.text.Text`) files:
+
+* :const:`SPEED_1` (slowest)
+* :const:`SPEED_2`
+* :const:`SPEED_3`
+* :const:`SPEED_4`
+* :const:`SPEED_5` (fastest)
+
+--------
+Examples
+--------
+
+Make a text file with fast text::
+
+  msg = alphasign.Text("%sthis text is fast" % alphasign.speeds.SPEED_5,
+                       label="A",
+                       mode=alphasign.modes.ROTATE)
+"""
+
 # Display speeds
 SPEED_1 = "\x15"
 SPEED_2 = "\x16"
